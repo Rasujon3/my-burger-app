92ea6e3e99c1087955d8a876962883b765648f17
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-10-31 02:53:00 +0600

    Fetching the orders
    Video: 14

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchOrders } from '../../redux/actionCreators';
 
-const Orders = (props) => {
-    return (
-        <div>
-            <h1>
-                orders
-            </h1>
-        </div>
-    );
-};
+const mapStateToProps = state => {
+    return {
+        orders: state.orders,
+        orderLoading: state.orderLoading,
+        orderErr: state.orderErr,
+    }
+}
 
-export default Orders;
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchOrders: () => dispatch(fetchOrders()),
+    }
+}
+
+class Orders extends Component {
+    componentDidMount() {
+        this.props.fetchOrders();
+    }
+    componentDidUpdate() {
+        console.log(this.props);
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Orders);
